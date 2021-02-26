78156ae9ce601c467f1043e9b58df0e632d92d2e
Shadee Merhi <shadmerhi@gmail.com>
2021-02-26 10:31:24 -0700

    remove api call and initial todo state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './styles/App.css';
 
-import Header from './components/Header';
+import Header from './components/Header'; 
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
 import CompletedTodos from './components/CompletedTodos';
 
 import axios from 'axios';
+
+const todoData = [
+  {
+    id: 1,
+    title: 'Todo Item 1',
+  },
+  {
+    id: 2,
+    title: 'Todo Item 2',
+  },
+  {
+    id: 3,
+    title: 'Todo Item 3',
+  }
+]
 
 
 function App() {
@@ -14,21 +29,21 @@ function App() {
   const apiURL = 'https://jsonplaceholder.typicode.com/todos';
   const apiLimit = '?_limit=5';
 
-  const [todos, setTodos] = useState([]);
-  const [completedTodos, setCompletedTodos] = useState([]);
+  // const [todos, setTodos] = useState([]);
+  // const [completedTodos, setCompletedTodos] = useState([]);
 
-  useEffect(() => {
-    axios.get(`${apiURL}${apiLimit}`).then(res => {
+  // useEffect(() => {
+  //   axios.get(`${apiURL}${apiLimit}`).then(res => {
 
-      res.data.forEach(todo => {
-        if(!todo.completed) {
-          setTodos(prev => [...prev, todo]);
-        } else {
-          setCompletedTodos(prev => [...prev, todo]);
-        }
-      });
-    })
-  }, []);
+  //     res.data.forEach(todo => {
+  //       if(!todo.completed) {
+  //         setTodos(prev => [...prev, todo]);
+  //       } else {
+  //         setCompletedTodos(prev => [...prev, todo]);
+  //       }
+  //     });
+  //   })
+  // }, []);
 
   return (
     <div className="app">
@@ -36,8 +51,8 @@ function App() {
       <div className="main-container">
         <AddTodo />
         <div className="todo-container">
-          <Todos todos={todos}/>
-          <CompletedTodos todos={completedTodos}/>
+          <Todos todos={todoData}/>
+          <CompletedTodos />
         </div>
       </div>
     </div>
