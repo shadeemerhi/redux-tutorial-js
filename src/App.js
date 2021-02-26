67067cf44021b38220a053f1aa8b5f0606051a5a
Shadee Merhi <shadmerhi@gmail.com>
2021-02-25 23:41:28 -0700

    api call made, todos state updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/App.css';
 
 import Header from './components/Header';
@@ -6,29 +6,30 @@ import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
 import CompletedTodos from './components/CompletedTodos';
 
-const todoList = [
-  {
-    userId: 1,
-    id: 1,
-    title: 'Todo Item 1',
-    completed: false
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: 'Todo Item 2',
-    completed: false
-  }
-]
+import axios from 'axios';
+
 
 function App() {
+
+  const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+  const apiLimit = '?_limit=5';
+
+  const [todos, setTodos] = useState([]);
+  const [completedTodos, setCompletedTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${apiURL}${apiLimit}`).then(res => {
+      setTodos(res.data);
+    })
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <div className="main-container">
         <AddTodo />
         <div className="todo-container">
-          <Todos todos={todoList}/>
+          <Todos todos={todos}/>
           <CompletedTodos />
         </div>
       </div>
