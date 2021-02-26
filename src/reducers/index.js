6b7223dee104c11e14e06569033bc46359bdbd71
Shadee Merhi <shadmerhi@gmail.com>
2021-02-26 10:32:33 -0700

    create combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,8 @@
 import { combineReducers } from 'redux';
+import todosReducer from '../reducers/todos';
+
+const allReducers = combineReducers({
+  todos: todosReducer
+});
+
+export default allReducers;
