927239ded207d685112d8e38afd2d31c3d549e66
Shadee Merhi <shadmerhi@gmail.com>
2021-02-26 10:33:08 -0700

    create redux store, wrap App component'

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,8 +1,19 @@
+import {
+  ADD_TODO,
+  COMPLETE_TODO,
+  DELETE_TODO,
+  READD_TODO
+} from '../actions/todos';
+
 const initialState = [];
 
 const todosReducer = (state = initialState, action) => {
   switch(action.type) {
+    case ADD_TODO:
+      return state;
     default:
       return state;
   }
 }
+
+export default todosReducer;
